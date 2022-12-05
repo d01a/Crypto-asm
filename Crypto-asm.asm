0e12d5005df77784606dc9bf723880f42960ea48
Ibrahim Yasser <ebrahim195016@feng.bu.edu.eg>
2022-12-05 19:35:07 +0200

    Refactor: change some code in the RC4_Output function, You should not debug now untill I make some tests

--- Crypto-asm.asm
+++ Crypto-asm.asm
@@ -10,6 +10,8 @@ ExitProcess PROTO, dwExitCode:dword
 .data 
 S			db		256 dup(?)				; Declaring uninitialized 256 char
 key			DB		"Secret",0
+plaintext	DB		256 dup(?)
+ciphertext	DB		256 dup(?)
 key_Length	DB		6
 i			DW		?
 j			DW		?
@@ -21,16 +23,7 @@ main PROC
 	call RC4_Init
 	call RC4_Output
 	pop ax
-	call RC4_Output
-	pop ax
-	call RC4_Output
-	pop ax
-	call RC4_Output
-	pop ax
-	call RC4_Output
-	pop ax
-	call RC4_Output
-	pop ax
+	
 
 	invoke ExitProcess,0
 main endp
@@ -71,7 +64,9 @@ RC4_Init PROC
 
 	L2:
 		mov ax,  i				;move the value of i to AX to be divided with key length and the value of the remainder will be stored in AH so shift right it with 8
+		;call strlen
 		div key_Length			;divide i with the keylength length 
+		
 		shr ax, 8				;move the remainder to al
 		mov bl ,[edi]			;as we can't make a memory to memory transfer, use register as a temp bl = S[i]
 		add j, bx				; j = j + S[i] 
@@ -90,9 +85,6 @@ RC4_Init PROC
 			i = j = 0;
 		^
 
-		mov i, 0				;i = 0
-		mov j, 0				;j = 0
-
 		ret
 RC4_Init ENDP
 ;RC4_Init Procedure end
@@ -100,46 +92,66 @@ RC4_Init ENDP
 
 Swap PROC
         
-		 mov edi, OFFSET S   ;Storing the start address of the array in the index register DI
-		 ;Make sure that all regs are empty
+		 mov edi, OFFSET S				;Storing the start address of the array in the index register DI
+										;Make sure that all regs are empty
 		 mov ebx,0            
 		 mov edx,0
 		 mov cx,0
 		 mov ax,0
-		 ;get the indexes we want to swap
+										;get the indexes we want to swap
 		 mov bx, i         
 		 mov dx, j
-	     mov al, [edi+ebx]   ;temp=s[i]
+	     mov al, [edi+ebx]				;temp=s[i]
 	     ;s[i]=s[j]
 		 mov cl,[edi+edx]    
 	     mov [edi+ebx],cl
-	     mov [edi+edx],al   ;s[j]=temp
+	     mov [edi+edx],al				;s[j]=temp
          ret
 Swap ENDP
 
 RC4_Output PROC
-		mov edi, OFFSET S
+		mov i, 0						;i = 0
+		mov j, 0						;j = 0
+		lea edi, S
 		xor eax,eax
 		xor ebx,ebx
+		call strlen
+		mov ecx,eax						;store length of text in ecx
+		xor edx, edx					;n (counter)
+	L_Out:
 		mov ax, i
 		mov bx, j
-		add bl, [edi+eax]		;j = j + S[i]
-		and bl, 255
-		mov j, bx
-		inc i					;i = i + 1
-		and i, 255				;i = (i + 1) & 255	
+
+		inc i							;i = i + 1
+		and i, 255						;i = (i + 1) & 255	
+		
+		mov ax, i						;ax=i
+		mov j, bx						;j = bx
+		add bl, [edi+eax]				;j = j + S[i]
+		and bl, 255						;j = (j + S[i]) & 255
+
 		pusha
-		call swap
-		popa					;S[(S[i] + S[j]) & 255]
-		mov ax, i
-		mov bx, j
-		mov al, [edi + eax]
-		mov bl, [edi + ebx]
-		add ax,bx
-		and ax,255
-		pop ecx
-		push ax
-		push ecx
+		call swap						;swap S[i],S[j]
+		popa							
+		
+		mov ax, i						;ax=i
+		mov bx, j						;bx=j
+
+		mov al, [edi + eax]				;al = S[i]
+		mov bl, [edi + ebx]				;bl = S[j]	
+		add ax,bx						;ax = S[i] + S[j]	
+		and ax,255						;ax = (S[i] + S[j]) & 255
+		mov al, [edi+eax]				;al = S[(S[i] + S[j])&255]	
+
+		lea esi, plaintext				;load base address of plaintext in esi
+		xor bx,bx						; bx = 0
+		mov bl, [esi + edx]				;bl = plaintext[n]	
+		xor ax,	bx						;S[(S[i] + S[j]) & 255] ^ plaintext[n]
+		lea esi, ciphertext				;load base address of ciphertext in esi
+		mov [esi + edx],al				;ciphertext[n] = S[(S[i] + S[j]) & 255] ^ plaintext[n]
+		inc dx							;n++
+		cmp dx,cx						;check if n-len=0 if so we will not jump to L_Out and this loop exit
+		jnz L_Out
 		ret
 
 RC4_Output ENDP
@@ -156,12 +168,12 @@ strlen proc
 	   xor ecx, ecx		   	   ;strlen = 0
 	   ;lea esi, [S]		   ;move base addr of S to esi -> **this line moved to the caller function
 	   jmp L2
-    L1:
+    L5:
 	   add ecx,1			   ; cx++
     L2: 
 	   mov edx, [esi + ecx]		   ; edx = s[ecx]
 	   test edx, edx		   ; testing if edx is zero, zf is set -> done b using bitwise AND 
-	   jne L1			   ; zf not set, string is not ended 
+	   jne L5			   ; zf not set, string is not ended 
 	   mov eax, ecx
 	   ret 
 ;-----------------------------------------------------------------------------;
