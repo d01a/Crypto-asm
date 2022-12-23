fe875b29cc398be5f69a7d18ff4da876de757dc3
Ibrahim Yasser <ebrahim195016@feng.bu.edu.eg>
2022-12-23 15:18:35 +0200

    docs: add C source code for RC4

--- Crypto-asm.c
+++ Crypto-asm.c
@@ -2,39 +2,59 @@
 
 /*-------------------------RC4-----------------------------*/
 // main function is stripped out for now
-unsigned char S[256];
-unsigned int i, j;
+#define N 256   // 2^8
 
-void swap(unsigned char *s, unsigned int i, unsigned int j) {
-    unsigned char temp = s[i];
-    s[i] = s[j];
-    s[j] = temp;
+void swap(unsigned char *a, unsigned char *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-/* KSA */
-void rc4_init(unsigned char *key, unsigned int key_length) {
-    for (i = 0; i < 256; i++)
+int KSA(char *key, unsigned char *S) {
+
+    int len = strlen(key);
+    int j = 0;
+
+    for(int i = 0; i < N; i++)
         S[i] = i;
- 
-    for (i = j = 0; i < 256; i++) {
-        j = (j + key[i % key_length] + S[i]) & 255;
-        swap(S, i, j);
+
+    for(int i = 0; i < N; i++) {
+        j = (j + S[i] + key[i % len]) % N;
+
+        swap(&S[i], &S[j]);
     }
 
-    i = j = 0;
+    return 0;
 }
 
- 
-/* PRGA */
-unsigned char rc4_output() {
-    i = (i + 1) & 255;
-    j = (j + S[i]) & 255;
+int PRGA(unsigned char *S, char *plaintext, unsigned char *ciphertext) {
 
-    swap(S, i, j);
+    int i = 0;
+    int j = 0;
 
-    return S[(S[i] + S[j]) & 255];
+    for(size_t n = 0, len = strlen(plaintext); n < len; n++) {
+        i = (i + 1) % N;
+        j = (j + S[i]) % N;
+
+        swap(&S[i], &S[j]);
+        int rnd = S[(S[i] + S[j]) % N];
+
+        ciphertext[n] = rnd ^ plaintext[n];
+
+    }
+
+    return 0;
 }
 
+int RC4(char *key, char *plaintext, unsigned char *ciphertext) {
+
+    unsigned char S[N];
+    KSA(key, S);
+
+    PRGA(S, plaintext, ciphertext);
+
+    return 0;
+}
 
 /*------------------------------ROT13-----------------------*/
 // text[] should be defined as the same array representing the input, e.g. S[] in RC4 should be the same as text[] here. Same 
